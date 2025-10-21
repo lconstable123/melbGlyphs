3e9baa3d3abee4626e5fcf6653731e29e350ca48
LUKECONSTABLE <luke@lukeconstable.com>
2025-10-22 09:46:19 +1100

    b

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
+
 import { imageSchema } from "./utils/schemas.js";
+import multer from "multer";
+import sharp from "sharp";
+import heicConvert from "heic-convert";
+import { fileTypeFromBuffer } from "file-type";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const StoredImages = [];
-
+const convertedImages = [];
+const upload = multer({ storage: multer.memoryStorage() });
 app.use(cors(), express.json());
 
 app.post("/images", (req, res) => {
@@ -33,37 +38,51 @@ app.delete("/images/:id", (req, res) => {
   const { id } = req.params;
   console.log(`Received a DELETE request to /images/${id}`);
 });
+//-----------------------------------------------------------------convert images endpoint
+app.post("/convert", upload.single("image"), async (req, res) => {
+  console.log("Received a POST request to /convert");
 
-app.post("/convert", (req, res) => {
-
-    try {
-        const inputPath = req.file.path;
-        const outPutFileName = `${req.file.filename}.jpg`
-        const outputPath  path.join("converted",outPutFileName);
-    } catch (error) {
-        console.error("Error processing conversion:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+  try {
+    if (!req.file) {
+      return res.status(404).json({ error: "File not found" });
     }
 
+    let buffer = req.file.buffer;
 
+    const fileType = await fileTypeFromBuffer(buffer);
+    console.log("Detected MIME type from content:", fileType?.mime);
+
+    if (fileType?.mime === "image/heic" || fileType?.mime === "image/heif") {
+      console.log("Converting HEIC/HEIF to JPEG...");
+      buffer = await heicConvert({
+        buffer,
+        format: "JPEG",
+        quality: 1,
+      });
+    }
+
+    const jpegBuffer = await sharp(buffer).jpeg().toBuffer();
+
+    const base64Image = jpegBuffer.toString("base64");
+
+    const imageObject = {
+      key: Date.now().toString(),
+      filename: req.file.originalname,
+      mimeType: "image/jpeg",
+      data: base64Image,
+    };
+
+    convertedImages.push(imageObject);
+
+    res.status(200).json({
+      message: "Image converted successfully",
+      image: imageObject,
+    });
+  } catch (error) {
+    console.error("Error processing conversion:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
